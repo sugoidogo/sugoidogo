17d8eb749023da1bef6a8c23867fbc9d21e0a0d5
josephsmendoza <josephsmendoza@gmail.com>
2023-12-03 16:48:54 -0800

    update build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 markdown README.md > index.html
-echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown-dark.css">' >> index.html
+echo '<link rel="stylesheet" href="https://raw.githubusercontent.com/markdowncss/modest/master/css/modest.css">' >> index.html
